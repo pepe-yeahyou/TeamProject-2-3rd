8f18f32fa3eb97e7bc096b6f70ecf5653a474b32
jkk <unfinefeeling5959@gmail.com>
2025-12-11 18:29:34 +0900

    commit init

---
+++ src/test/java/com/example/myteam/MyteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
